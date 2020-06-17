b02268b48819b5fceb32c424ef4d15b03ba71682
berb00 <berb11@163.com>
2020-06-17 23:36:30 +0800

    1

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
-module go
+module Go
 
 go 1.14
+
+//require base v0.0.1
+
+//replace base v0.0.1 => ./base
--- main.go
+++ main.go
@@ -2,11 +2,11 @@
 // 指明这个文件属于哪个包，package main表示一个可独立执行的程序，每个 Go 应用程序都包含一个名为 main 的包。
 package main
 
-// 引入包
-import "./base" // base 目录
-// import "github.com/jinzhu/configor" //
-// import "./math" // math 目录
-// import "fmt"    // 格式化 IO（输入/输出）
+import (
+	"Go/base"
+	"Go/math"
+	"fmt"
+)
 
 // 初始化函数，若存在会在main函数之前执行
 func init() {
@@ -15,12 +15,12 @@ func init() {
 
 // 入口函数 自动调用
 func main() { // { 不能另起一行
-	// 
+
 	// fmt.Println("外部包测试：", configor.Config{})
 
 	// math
-	// fmt.Println(math.Add(1, 1))
-	// fmt.Println(math.Sub(1,1))
+	fmt.Println(math.Add(1, 1))
+	fmt.Println(math.Sub(1, 1))
 
 	// var
 	// base.VarDefault()
