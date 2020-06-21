c1dbb07a15d4d4ebd71b37cee0f362ea3eb0eddb
berb00 <berb11@163.com>
2020-06-21 23:15:27 +0800

    success for go mod vendor

--- main.go
+++ main.go
@@ -3,9 +3,11 @@
 package main
 
 import (
-	"Go/base"
-	_ "Go/math"
-	_ "fmt"
+	"fmt"
+	"github.com/shopspring/decimal"
+	"github.com/tidwall/gjson"
+	"golang/base"
+	_ "golang/math"
 	// _ "fmt"		// _指匿名包
 	// F "fmt"		// 给包起别名为
 )
@@ -43,4 +45,41 @@ func main() { // { 不能另起一行
 	// struct
 	// base.InitStruct()
 	base.TestStructFunc()
+
+	//
+	testPackage()
+	testDecimal()
+}
+
+func testPackage() {
+	const json = `{"name":{"first":"Janet","last":"Prichard"},"age":47}`
+
+	value := gjson.Get(json, "name.last")
+	fmt.Println(value.String())
+
+}
+
+func testDecimal() {
+
+	price, err := decimal.NewFromString("136.02")
+	if err != nil {
+		panic(err)
+	}
+
+	quantity := decimal.NewFromInt(3)
+
+	fee, _ := decimal.NewFromString(".035")
+	taxRate, _ := decimal.NewFromString(".08875")
+
+	subtotal := price.Mul(quantity)
+
+	preTax := subtotal.Mul(fee.Add(decimal.NewFromFloat(1)))
+
+	total := preTax.Mul(taxRate.Add(decimal.NewFromFloat(1)))
+
+	fmt.Println("Subtotal:", subtotal)                      // Subtotal: 408.06
+	fmt.Println("Pre-tax:", preTax)                         // Pre-tax: 422.3421
+	fmt.Println("Taxes:", total.Sub(preTax))                // Taxes: 37.482861375
+	fmt.Println("Total:", total)                            // Total: 459.824961375
+	fmt.Println("Tax rate:", total.Sub(preTax).Div(preTax)) // Tax rate: 0.08875
 }
